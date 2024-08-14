5bd3bac11b693503e43bf368ecced16021b74485
Ankoay-Feno <ankoayfeno@gmail.com>
2024-08-14 15:06:33 +0300

    class to read the json file

--- app/src/main/java/mg/geit/DJ/quiz/JsonNedded.kt
+++ app/src/main/java/mg/geit/DJ/quiz/JsonNedded.kt
@@ -1,4 +1,23 @@
 package mg.geit.DJ.quiz
 
-class JsonNedded {
+import android.content.Context
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+
+ class JsonNedded {
+    // Fonction pour lire le contenu JSON du fichier
+    private fun readJsonFromAssets(context: Context, fileName: String): String? {
+        return try {
+            val inputStream: InputStream = context.assets.open(fileName)
+            val reader = BufferedReader(InputStreamReader(inputStream))
+            val sb = StringBuilder()
+            reader.forEachLine { sb.append(it) }
+            reader.close()
+            sb.toString()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
 }
