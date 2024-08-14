d76370507219c05c42b7a34123ef372f3a290d25
Ankoay-Feno <ankoayfeno@gmail.com>
2024-08-15 00:22:43 +0300

    class to manage the json

--- app/src/main/java/mg/geit/DJ/quiz/CategoriesProfils.kt
+++ app/src/main/java/mg/geit/DJ/quiz/CategoriesProfils.kt
@@ -1,4 +1,13 @@
 package mg.geit.DJ.quiz
 
-class CategoriesProfils {
-}
+data class CategoriesProfils(
+    val bienfaiteur: List<String>?,
+    val insoumis: List<String>?,
+    val visionnaire: List<String>?,
+    val célébrité: List<String>?,
+    val médiateur: List<String>?,
+    val conservateur: List<String>?,
+    val épicurien: List<String>?,
+    val leader: List<String>?
+)
+{}
